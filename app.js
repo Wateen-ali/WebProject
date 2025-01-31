1fc4b20e71711121572469ea350a865c75bcea41
mh73d <mhkook7@gmail.com>
2025-01-31 19:32:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const express= require('express');
+const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./server/config/db');
 const session = require('express-session');
@@ -8,44 +8,46 @@ const passport = require('passport');
 const MongoStore = require('connect-mongo');
 
 const app = express();
-const port = 5001 || process.env.PORT;
+const PORT = process.env.PORT || 5001;
+const HOST = '0.0.0.0'; // Allows access from other devices
+
+// Session setup
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI
-
     }),
-    //cookie: { maxAge: new Date ( Date.now() + (3600000))}
-    
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 connectDB();
-//static files
+
+// Static files
 app.use(express.static('public'));
-//tamplating engine
+
+// Templating engine
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-
+// Routes
 app.use('/', require('./server/screens/auth'));
 app.use('/', require('./server/screens/index'));
 app.use('/', require('./server/screens/homescreen'));
 
+// Handle 404
+app.get('*', (req, res) => {
+    res.status(404).render('404');
+});
 
-//Handle 404
-app.get('*',function(req,res){
-    // res.status(404).send('404 Page Not Found')
-     res.status(404).render('404');
- })
-app.listen(port, ()=> {
-    console.log(`App listening on port ${port}`);
+// Start server
+app.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
 });
