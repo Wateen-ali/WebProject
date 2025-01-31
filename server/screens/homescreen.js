b58a73d70b5c95e69dbe937aea762ab61a714a66
mh73d <mhkook7@gmail.com>
2025-01-31 10:36:33 +0300

    Update homescreen.js

--- server/screens/homescreen.js
+++ server/screens/homescreen.js
@@ -3,5 +3,20 @@ const routes= express.Router();
 const { isLoggedIn } = require('../middleware/cheackAuth');
 const homescreenController = require('../controllers/homescreenController');
 
+
 routes.get('/homescreen',isLoggedIn, homescreenController.homescreen);
+
+
+routes.get('/homescreen/item/:id', isLoggedIn, homescreenController.homescreenViewJournal);
+routes.post('/homescreen/item/:id', isLoggedIn, homescreenController.homescreenUpdateJournal);
+routes.post('/homescreen/item-delete/:id', isLoggedIn, homescreenController.homescreenDeleteJournal);
+routes.get('/homescreen/add', isLoggedIn, homescreenController.homescreenAddJournal);
+routes.post('/homescreen/add', isLoggedIn, homescreenController.homescreenAddJournalSubmit);
+routes.get('/homescreen/search', isLoggedIn, homescreenController.homescreenSearch);
+routes.post('/homescreen/search', isLoggedIn, homescreenController.homescreenSearchSubmit);
+
+
+
+
+
 module.exports = routes;
