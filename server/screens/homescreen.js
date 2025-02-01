76332ea0afff8b0af3413cbc591d0df70a529677
Renad9x <148865745+Renad9x@users.noreply.github.com>
2025-02-01 19:44:25 +0300

    Update homescreen.js

--- server/screens/homescreen.js
+++ server/screens/homescreen.js
@@ -6,7 +6,6 @@ const homescreenController = require('../controllers/homescreenController');
 
 routes.get('/homescreen',isLoggedIn, homescreenController.homescreen);
 
-
 routes.get('/homescreen/item/:id', isLoggedIn, homescreenController.homescreenViewJournal);
 routes.post('/homescreen/item/:id', isLoggedIn, homescreenController.homescreenUpdateJournal);
 routes.post('/homescreen/item-delete/:id', isLoggedIn, homescreenController.homescreenDeleteJournal);
@@ -15,8 +14,11 @@ routes.post('/homescreen/add', isLoggedIn, homescreenController.homescreenAddJou
 routes.get('/homescreen/search', isLoggedIn, homescreenController.homescreenSearch);
 routes.post('/homescreen/search', isLoggedIn, homescreenController.homescreenSearchSubmit);
 
+// Profile routes
+routes.get('/homescreen/profile', isLoggedIn, homescreenController.profile);
+routes.post('/homescreen/profile', isLoggedIn, homescreenController.profile);
 
-
-
+routes.get('/homescreen/upprofile', isLoggedIn, homescreenController.upprofile);
+routes.post('/homescreen/upprofile', isLoggedIn, homescreenController.upprofile);
 
 module.exports = routes;
