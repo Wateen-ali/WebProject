3a0cbb419cde6bc054241aee09a078c68fbd876e
Renad9x <148865745+Renad9x@users.noreply.github.com>
2025-02-01 19:44:52 +0300

    Update index.js

--- server/screens/index.js
+++ server/screens/index.js
@@ -2,11 +2,7 @@ const express= require('express');
 const routes= express.Router();
 const mainController = require('../controllers/mainController');
 
-
-
-
 routes.get('/', mainController.homepage);
 routes.get('/about', mainController.about);
-
 
 module.exports = routes;
