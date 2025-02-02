85da0c44d530a42ca5d3c9db51a2709f4508ab55
mh73d <mhkook7@gmail.com>
2025-02-02 09:23:42 +0300

    Update homescreenController.js

--- server/controllers/homescreenController.js
+++ server/controllers/homescreenController.js
@@ -1,8 +1,6 @@
 const Journal = require('../models/Journals');
-const User = require('../models/User');
-const isLoggedIn = require('../middleware/cheackAuth');
-const express = require('express');
 const mongoose = require('mongoose');
+
 
 exports.homescreen = async (req, res, next) => {
     const perPage = 12;
@@ -12,11 +10,13 @@ exports.homescreen = async (req, res, next) => {
         title: "home screen ",
         description: "our website homepage.",
     };
+
     try {
         // Ensure user ID is valid
         if (!mongoose.Types.ObjectId.isValid(req.user.id)) {
             return res.status(400).send("Invalid user ID format.");
         }
+
         const userId = new mongoose.Types.ObjectId(req.user.id);
 
         // Fetch journals with pagination and filtering
@@ -31,7 +31,7 @@ exports.homescreen = async (req, res, next) => {
                 $project: {
                     title: { $substr: ['$title', 0, 30] },
                     body: { $substr: ['$body', 0, 100] },
-                    date: 1
+                    date:1
                 },
             },
         ])
@@ -40,6 +40,7 @@ exports.homescreen = async (req, res, next) => {
 
         // Count total journals for pagination
         const totalJournals = await Journal.countDocuments({ user: userId });
+
         res.render('homescreen/index', {
             userName: req.user.firstName,
             locals,
@@ -54,56 +55,85 @@ exports.homescreen = async (req, res, next) => {
     }
 }
 
-// View spescfic journal
-exports.homescreenViewJournal = async (req, res) => {
-    const journal = await Journal.findById({ _id: req.params.id })
-        .where({ user: req.user.id }).lean();
 
-    if (journal) {
+
+ // View spescfic journal
+
+exports.homescreenViewJournal = async(req,res) => {
+    const journal = await Journal.findById({ _id : req.params.id})
+    .where({ user : req.user.id}).lean();
+
+    if (journal){
         res.render('homescreen/viewJournals', {
-            journalID: req.params.id,
+            journalID : req.params.id ,
             journal,
-            layout: '../views/layouts/homescreen'
+            layout : '../views/layouts/homescreen'
+
         });
 
-    } else {
+    }else {
         res.send("Something went wrong.")
     }
+
+
 }
 
-//Update specfic note
-exports.homescreenUpdateJournal = async (req, res) => {
+
+ //Update specfic note
+ 
+exports.homescreenUpdateJournal = async(req,res) => {
     try {
         await Journal.findOneAndUpdate(
-            { _id: req.params.id },
-            { title: req.body.title, body: req.body.body, date: req.body.date, updatedAt: Date.now() }
+            { _id : req.params.id},
+            {title : req.body.title , body: req.body.body ,  date: req.body.date, updatedAt: Date.now()}
         ).where({ user: req.user.id });
         res.redirect('/homescreen');
 
+        
     } catch (error) {
         console.log(error);
+        
     }
+    
+
+
 }
 
-//Delete note
-exports.homescreenDeleteJournal = async (req, res) => {
+
+
+ //Delete note
+ 
+
+exports.homescreenDeleteJournal = async (req,res) => {
     try {
-        await Journal.deleteOne({ _id: req.params.id }).where({ user: req.user.id });
+        await Journal.deleteOne({ _id : req.params.id}).where({ user: req.user.id});
         res.redirect('/homescreen')
+        
     } catch (error) {
         console.log(error);
+        
     }
 }
 
-//ADD note
-exports.homescreenAddJournal = async (req, res) => {
+
+
+ //ADD note
+ 
+
+
+exports.homescreenAddJournal = async (req,res) =>{
     res.render('homescreen/add', {
         layout: '../views/layouts/homescreen'
     });
 }
 
-exports.homescreenAddJournalSubmit = async (req, res) => {
+
+
+
+
+exports.homescreenAddJournalSubmit = async (req,res) => {
     try {
+
         req.body.user = req.user.id;
         const journalData = {
             title: req.body.title,
@@ -111,47 +141,62 @@ exports.homescreenAddJournalSubmit = async (req, res) => {
             user: req.user.id,
             date: req.body.date || new Date() // Store date properly
         };
+
         await Journal.create(journalData);
         res.redirect('/homescreen');
+        
     } catch (error) {
         console.log(error);
-
+        
     }
 }
 
-//search
-exports.homescreenSearch = async (req, res) => {
+
+
+
+ //search
+ 
+
+exports.homescreenSearch = async (req,res) => {
     try {
         res.render('homescreen/search', {
             searchResult: '',
             layout: '../views/layouts/homescreen'
         })
-
-    } catch (error) {}
+        
+    } catch (error) {
+        
+  
+  
+    }
 }
+
+
 
 exports.homescreenSearchSubmit = async (req, res) => {
     try {
-        let searchTerm = req.body.searchTerm;
-        const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
-
-        const searchResults = await Journal.find({
-            $or: [
-                { title: { $regex: new RegExp(searchNoSpecialChars, "i") } },
-                { body: { $regex: new RegExp(searchNoSpecialChars, "i") } },
-            ],
-        }).where({ user: req.user.id });
-
-        res.render("homescreen/search", {
-            searchResults,
-            layout: "../views/layouts/homescreen",
-        });
+      let searchTerm = req.body.searchTerm;
+      const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+  
+      const searchResults = await Journal.find({
+        $or: [
+          { title: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+          { body: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+        ],
+      }).where({ user: req.user.id });
+  
+      res.render("homescreen/search", {
+        searchResults,
+        layout: "../views/layouts/homescreen",
+      });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-};
+  };
 
-// progile Screens:
+
+
+  // progile Screens:
 exports.profile = async (req, res ) => {
     const locals = {
         title: "progile screen ",
@@ -173,3 +218,8 @@ exports.upprofile = async (req, res ) => {
         userName: req.user.displayName, 
        });
 }
+
+
+
+
+
