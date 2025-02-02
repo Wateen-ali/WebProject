c02bc07ca2ae641cce7b37f23bdb981191310b47
mh73d <mhkook7@gmail.com>
2025-02-02 09:24:39 +0300

    Update Journals.js

--- server/models/Journals.js
+++ server/models/Journals.js
@@ -17,6 +17,13 @@ const JournalSchema = new Schema ({
         required : true ,
          
     },
+
+
+    date:{
+        type :Date ,
+        required : true ,
+
+    },
     createdAt :{
         type :Date ,
         default : Date.now() 
